8807c1cf4d4f118cfb7b044189701ae63d9c3885
Simon Walker-Samuel <simon.walkersamuel@ucl.ac.uk>
2023-08-18 12:32:25 +0100

    updated to absolute paths in config

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
-# REANIMATE (flow estimation) directories
-REANIMATE_DIR = "Reanimate/Reanimate" 
-#REANIMATE_DATA_FILENAME = "/PATH/TO/REANIMATE/DAT_FILE.dat"
-REANIMATE_TMP_DIR_LOC = "~temp"
+import os
+ROOT_DIR = os.path.dirname(__file__)
 
-RETINAGEN_DIR = "RetinaGen/RetinaGen/bin/Release/net6.0"
-RETINAGEN_DATA_DIR = "data/RetinaGen"
+# REANIMATE (flow estimation) directories
+REANIMATE_DIR = os.path.join(ROOT_DIR,"Reanimate/Reanimate")
+#REANIMATE_DATA_FILENAME = "/PATH/TO/REANIMATE/DAT_FILE.dat"
+REANIMATE_TMP_DIR_LOC = os.path.join(ROOT_DIR,"temp")
+
+RETINAGEN_DIR = os.path.join(ROOT_DIR,"RetinaGen/RetinaGen/bin/Debug/net6.0")
+RETINAGEN_DATA_DIR = os.path.join(ROOT_DIR,"data/RetinaGen")
